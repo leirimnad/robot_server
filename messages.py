62d0ea88552fe50811407b761974ce8994ae2ee7
Daniil Myronov <myrondan@fit.cvut.cz>
2023-03-31 15:32:20 +0200

    restructure client messages checks

--- messages.py
+++ messages.py
@@ -1,19 +1,50 @@
-class _ServerMessages:
-    @property
-    def server_key_request(self) -> bytes: return b"107 KEY REQUEST"
+import re
 
-    @property
-    def server_ok(self) -> bytes: return b"200 OK"
 
-    @property
-    def server_key_out_of_range_error(self) -> bytes: return b"303 KEY OUT OF RANGE"
-
-    @property
-    def server_syntax_error(self) -> bytes: return b"301 SYNTAX ERROR"
+class ServerMessages:
+    SERVER_KEY_REQUEST = b"107 KEY REQUEST"
+    SERVER_OK = b"200 OK"
+    SERVER_KEY_OUT_OF_RANGE_ERROR = b"303 KEY OUT OF RANGE"
+    SERVER_SYNTAX_ERROR = b"301 SYNTAX ERROR"
+    SERVER_LOGIN_FAILED = b"300 LOGIN FAILED"
+    SERVER_MOVE = b"102 MOVE"
+    SERVER_TURN_LEFT = b"103 TURN LEFT"
+    SERVER_TURN_RIGHT = b"104 TURN RIGHT"
 
     @staticmethod
     def server_confirmation(server_hash: int) -> bytes:
         return str(server_hash).encode()
 
 
-server_messages = _ServerMessages()
+class ClientMessages:
+
+    def __init__(self, end_sequence, arg_name):
+        self.end_sequence = end_sequence
+        self.arg_name = arg_name
+
+    def _re_check(self, re_str, add_end=True, full_match=True, **kwargs):
+        if self.arg_name not in kwargs.keys():
+            raise NameError(f'"{self.arg_name}" not in kwargs')
+        if full_match:
+            return re.fullmatch(re_str + (self.end_sequence if add_end else b''), kwargs.get(self.arg_name)) is not None
+        return re.match(re_str + (self.end_sequence if add_end else b''), kwargs.get(self.arg_name)) is not None
+
+    def username(self, **kwargs):
+        return self._re_check(b".{1,20}", **kwargs) \
+            and not self._re_check(self.end_sequence + b".", add_end=False, full_match=False, **kwargs)
+
+    def key_id(self, **kwargs):
+        return self._re_check(b"[01234]", **kwargs)
+
+    def key_id_syntax(self, **kwargs):
+        return self._re_check(b"(-|\d)?\d?\d", **kwargs)
+
+    def confirmation_syntax(self, **kwargs):
+        return self._re_check(b"\d{1,5}", **kwargs)
+
+    def ok(self, **kwargs):
+        return self._re_check(b"OK -?\d{1,4} -?\d{1,4}", **kwargs) and self._re_check(b".{1,12}", **kwargs)
+
+    def parse_position(self, **kwargs):
+        res = re.fullmatch(b"OK (-?\d{1,4}) (-?\d{1,4})" + self.end_sequence, kwargs.get(self.arg_name))
+        return res.group(1), res.group(2)
--- run.py
+++ run.py
@@ -2,17 +2,7 @@ import socket
 import re
 from threading import Thread
 from transitions import Machine
-from messages import server_messages
-
-
-def re_check(re_str, arg_name="message"):
-    def func(**kwargs):
-        if arg_name not in kwargs.keys():
-            raise NameError(f'"{arg_name}" not in kwargs')
-        return re.match(re_str, kwargs.get(arg_name)) is not None
-
-    return func
-
+from messages import ServerMessages, ClientMessages
 
 server_keys = {
     0: 23019,
@@ -32,8 +22,9 @@ client_keys = {
 
 
 class RobotThread(Thread):
-    end_sequence = b"ZZ"
-    states = ['wait_username', 'wait_key_id', 'wait_confirmation', 'wait_client_ok', 'final']
+    end_sequence = b"\a\b"
+    states = ['wait_username', 'wait_key_id', 'wait_confirmation', 'wait_initial_client_ok', 'wait_client_ok', 'final']
+    client_checks = ClientMessages(end_sequence=end_sequence, arg_name="message")
 
     def __init__(self, connection, address):
         Thread.__init__(self)
@@ -44,42 +35,53 @@ class RobotThread(Thread):
         self.key_id = None
         self.server_hash = None
         self.stop_flag = False
+        self.position = None
+        self.rotation = None
 
         self.machine = Machine(model=self, states=RobotThread.states, initial='wait_username')
-        self.machine.add_transition('process_message',
-                                    'wait_username', 'wait_key_id',
-                                    unless=[re_check(self.end_sequence + b".")],
-                                    conditions=[re_check(b".{1,20}" + self.end_sequence)])
+        self.machine.add_transition('process_message', 'wait_username', 'wait_key_id',
+                                    conditions=self.client_checks.username)
 
-        self.machine.add_transition('process_message',
-                                    'wait_key_id', 'wait_confirmation',
-                                    conditions=[re_check(b"(-|\d)?\d?\d" + self.end_sequence),
-                                                re_check(b"[01234]" + self.end_sequence)]
-                                    )
-        self.machine.add_transition('process_message',
-                                    'wait_key_id', 'final',
-                                    conditions=[re_check(b"(-|\d)?\d?\d" + self.end_sequence)],
-                                    before=lambda **kwargs: self.send(server_messages.server_key_out_of_range_error)
+        self.machine.add_transition('process_message', 'wait_key_id', 'wait_confirmation',
+                                    conditions=self.client_checks.key_id)
+        self.machine.add_transition('process_message', 'wait_key_id', 'final',
+                                    conditions=self.client_checks.key_id_syntax,
+                                    before=lambda **kwargs: self.send(ServerMessages.SERVER_KEY_OUT_OF_RANGE_ERROR)
                                     )
 
+        self.machine.add_transition('process_message', 'wait_confirmation', 'wait_initial_client_ok',
+                                    conditions=self.check_client_hash)
+        self.machine.add_transition('process_message', 'wait_confirmation', 'final',
+                                    conditions=self.client_checks.confirmation_syntax,
+                                    before=lambda **kwargs: self.send(ServerMessages.SERVER_LOGIN_FAILED))
+
+        self.machine.add_transition('process_message', 'wait_initial_client_ok', 'wait_client_ok',
+                                    conditions=self.client_checks.ok)
+
         self.machine.add_transition('process_message',
-                                    'wait_confirmation', 'wait_client_ok',
-                                    conditions=[self.check_client_hash,
-                                                re_check(b"[01234]" + self.end_sequence)]
-                                    )
-        self.machine.add_transition('process_message', ['wait_username', 'wait_key_id', 'wait_confirmation'], 'final',
-                                    before=lambda **kwargs: self.send(server_messages.server_syntax_error))
+                                    "*", 'final',
+                                    before=lambda **kwargs: self.send(ServerMessages.SERVER_SYNTAX_ERROR))
 
     def on_enter_wait_key_id(self, **kwargs):
         self.robot_username = (re.match(b"(.{1,20})" + self.end_sequence, kwargs.get("message")).group(1)).decode()
-        self.send(server_messages.server_key_request)
+        self.send(ServerMessages.SERVER_KEY_REQUEST)
 
     def on_enter_wait_confirmation(self, **kwargs):
         self.key_id = int(re.match(b"([01234])" + self.end_sequence, kwargs.get("message")).group(1))
         self.server_hash = self.compute_server_hash(self.robot_username, server_keys.get(self.key_id))
-        self.send(server_messages.server_confirmation(self.server_hash))
+        self.send(ServerMessages.server_confirmation(self.server_hash))
 
-    def on_enter_final(self):
+    def on_enter_wait_initial_client_ok(self, **kwargs):
+        self.send(ServerMessages.SERVER_OK)
+        self.send(ServerMessages.SERVER_MOVE)
+
+    def on_enter_wait_client_ok(self, **kwargs):
+        new_position = self.client_checks.parse_position(**kwargs)
+        if new_position == self.position:  # obstacle
+            pass
+        
+
+    def on_enter_final(self, **kwargs):
         self.conn.close()
         self.stop_flag = True
         print(f"{self.address} Disconnected, stopping thread.")
